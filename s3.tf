daea6713e12fb03748e0978e614ecea64bebfdf7
aakashvishwakarma <50501190+aakashvishwakarma@users.noreply.github.com>
2021-06-30 17:58:41 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "b" {
   bucket = "${var.bucket_name}"
   acl    = "${var.acl}"
-  region = "us-east-1"
+  region = "${var.region}"
 
   tags = {
     Name        = "My bucket"
